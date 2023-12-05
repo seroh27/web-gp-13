bc5d06c14ee6ee135c8b50a52fdf62a35997835e
Sero Hartounian <sero_h27@yahoo.com>
2023-12-05 12:36:23 +0330

    postgres configs added to setting.py

--- web-project/WeightLossApp/WeightLossApp/settings.py
+++ web-project/WeightLossApp/WeightLossApp/settings.py
@@ -76,9 +76,11 @@ WSGI_APPLICATION = 'WeightLossApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'laghariDatabase',
+        'USER': 'django-control',
+        'PASSWORD': 'admin',
     }
 }
 
