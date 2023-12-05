3fa2d062fcc4a5810b59940fc862abf2cb34a28a
Sero Hartounian <sero_h27@yahoo.com>
2023-12-05 11:48:08 +0330

    registered models in admin panel

--- web-project/WeightLossApp/WeightLossApp/urls.py
+++ web-project/WeightLossApp/WeightLossApp/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('control/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
--- web-project/WeightLossApp/user/admin.py
+++ web-project/WeightLossApp/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from user.models import User
+from user.models import Meal
+
+admin.site.register(User)
+admin.site.register(Meal)
