1e80cb97841de295ad4ff3d3d596c71ccd5e742a
Sero Hartounian <sero_h27@yahoo.com>
2023-12-06 10:42:48 +0330

    added get and post to user views

--- web-project/WeightLossApp/control/views.py
+++ web-project/WeightLossApp/control/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from django.http import  JsonResponse
 from control.models import Food
 from .serializer import FoodSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+@api_view(['GET','POST'])
 def food_list(request):
-    foods = Food.objects.all()
-    food_serializer = FoodSerializer(foods,many = True)
-    return JsonResponse({"foodlist",food_serializer.data})
+    if(request.method == 'GET'):
+        foods = Food.objects.all()
+        food_serializer = FoodSerializer(foods,many = True)
+        return JsonResponse({"foodlist":food_serializer.data})
+    if (request.method == 'POST'):
+        serializer = FoodSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
 # Create your views here.
