1a20e0a2ae1e3ccee74449a8f7019825ca8c0d66
seenpeh <seenpeh@gmail.com>
2024-02-04 17:09:43 +0330

    fixed the BMI page

--- frontend/vite-project/src/components/pages/UserPage/CalorieCalc.tsx
+++ frontend/vite-project/src/components/pages/UserPage/CalorieCalc.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Gradient from '../../gradient';
+import './CalorieCalc.css';
 
 function CalorieCalc() {
     const [heightValue, setHeightValue] = useState('');
@@ -15,21 +16,24 @@ function CalorieCalc() {
 
             let message = '';
             if (bmi < 18.5) {
+                const idealWeight = 18.5 * heightInMeters * heightInMeters;
+                const overWeight = (idealWeight - weightValue).toFixed(1);
+                message = ' شما حدود ' + overWeight + ' کیلوگرم کمبود وزن دارید '
                 message = 'شما کمبود وزن دارید';
             } else if (bmi >= 18.5 && bmi < 25) {
                 message = 'شما وزن نرمالی دارید';
             } else if (bmi >= 25 && bmi < 30) {
                 const idealWeight = 25 * heightInMeters * heightInMeters;
-                const overWeight = weightValue - idealWeight;
-                message = ' شما حدود' + overWeight + 'کیلوگرم اضافه وزن دارید '
+                const overWeight = (weightValue - idealWeight).toFixed(1);
+                message = ' شما حدود ' + overWeight + ' کیلوگرم اضافه وزن دارید '
             } else if (bmi >= 30 && bmi < 40) {
                 const idealWeight = 25 * heightInMeters * heightInMeters;
-                const overWeight = weightValue - idealWeight;
-                message = ' شما حدود' + overWeight + 'کیلوگرم دچار چاقی هستید '
+                const overWeight = (weightValue - idealWeight).toFixed(1);
+                message = ' شما حدود ' + overWeight + ' کیلوگرم دچار چاقی هستید '
             } else {
                 const idealWeight = 25 * heightInMeters * heightInMeters;
-                const overWeight = weightValue - idealWeight;
-                message = ' شما حدود' + overWeight + 'کیلوگرم دچار چاقی مفرط هستید '
+                const overWeight = (weightValue - idealWeight).toFixed(1);
+                message = ' شما حدود ' + overWeight + ' کیلوگرم دچار چاقی مفرط هستید '
             }
             setBmiMessage(message);
         } else {
@@ -38,82 +42,74 @@ function CalorieCalc() {
         }
     };
 
-    return (
-        <div className='flex flex-col items-center justify-center mt-10 mb-10'>
-            <div dir='rtl' className='w-11/12'>
-                <Gradient />
-                <h1 className='boldfont place-content-center'>شاخص توده بدنی چیست؟</h1>
-                <p dir='rtl'>شاخص توده بدنی (BMI) یک اندازه‌گیری است که استفاده می‌شود تا به طور تقریبی نشان دهد که یک فرد در چه حالتی از لحاظ وزنی قرار دارد. این شاخص با تقسیم وزن بدن به قد مربع شما به دست می‌آید و به عنوان یک اندازه‌گیری ساده و مقیاس‌پذیر برای ارزیابی وزن بدن استفاده می‌شود.
-                    <br /><br />
-                    بر اساس مقادیر BMI، افراد به چهار دسته تقسیم می‌شوند:
-                    <br />
-                    ۱. زیر وزن: BMI کمتر از ۱۸.۵<br />
-                    ۲. وزن نرمال: BMI بین ۱۸.۵ تا ۲۴.۹<br />
-                    ۳. اضافه وزن: BMI بین ۲۵ تا ۲۹.۹<br />
-                    ۴. چاقی: BMI بیشتر از ۳۰<br />
-                    <br />
-                    استفاده از شاخص توده بدنی به دلیل زیر موارد مفید است:
-                    <br />
-                    ۱. تشخیص ریسک‌های بهداشتی: مطالعات نشان داده‌اند که BMI با برخی از بیماری‌های مزمن مانند دیابت نوع دو، بیماری‌های قلبی عروقی و برخی از سرطان‌ها مرتبط است.<br />
-                    ۲. مشخص کردن برنامه‌های رژیم غذایی و ورزشی: با توجه به مقدار BMI، افراد می‌توانند برنامه‌های غذایی و ورزشی خود را تنظیم کنند تا به بهترین وضعیت وزنی برسند.<br />
-                    ۳. ارزیابی تغییرات وزن: اندازه‌گیری BMI می‌تواند به افراد کمک کند تا تغییرات وزن خود را مانیتور کنند و درصورت لزوم تصمیم‌گیری درباره تغییراتی که در رژیم غذایی و ورزشی‌شان ایجاد کنند، را داشته باشند.<br />
+    return (<>
+        <div dir='rtl' className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100 font pb-60">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    شاخص توده بدنی خود را محاسبه کنید
+                </h2>
+            </div>
 
-                    به هرحال، مهم است به این نکته توجه کنید که BMI تنها یک اندازه‌گیری ساده است و به تنهایی نمی‌تواند تمامیت مشکلات وزن و بهداشت را به صورت دقیق پوشش دهد. در نظر داشته باشید که عوامل دیگری مانند ترکیب بدنی (نسبت چربی به عضله)، فعالیت‌های ورزشی، الگوی غذایی و تاریخچه بیماری‌ها نیز نقش مهمی در بهبود و حفظ سلامتی و وزن ایده‌آل دارند.</p>
-                <div className="flex justify-center items-center pt-8">
-                    <div>
-                        <div className='w-40'>
-                            <label dir='rtl' htmlFor="height" className="block text-sm font-medium leading-6 text-gray-900">
-                                قد (سانتی‌متر)
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+                <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+                    <form className="space-y-8">
+                        <div>
+                            <label htmlFor="height" className="block text-sm font-medium leading-6 text-gray-900">
+                                قد (سانتی متر)
                             </label>
                             <div className="mt-2">
                                 <input
-                                    type="height"
-                                    name="height"
                                     id="height"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
-                                    onChange={(e) => setHeightValue(e.target.value)}
+                                    name="height"
+                                    type="height"
                                     value={heightValue}
+                                    onChange={(e) => setHeightValue(e.target.value)}
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
-                        <div className='pt-8 w-40'>
-                            <label dir='rtl' htmlFor="weight" className="block text-sm font-medium leading-6 text-gray-900">
+
+                        <div>
+                            <label htmlFor="weight" className="block text-sm font-medium leading-6 text-gray-900">
                                 وزن (کیلوگرم)
                             </label>
                             <div className="mt-2">
                                 <input
-                                    type="weight"
-                                    name="weight"
-                                    id="weight"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
                                     onChange={(e) => setWeightValue(e.target.value)}
                                     value={weightValue}
+                                    id="weight"
+                                    name="weight"
+                                    type="weight"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
-                        <div className='pt-8'>
-                            <button
-                                type="button"
-                                onClick={calculateBmi}
-                                className="mt-8 rounded bg-emerald-600 px-4 py-1 pt-2 pb-2 text-xs font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-                            >
-                                محاسبه شاخص توده بدنی
-                            </button>
-                            {bmiValue && bmiMessage && (
-                                <div className="result">
-                                    <p>
-                                        شاخص توده بدنی شما: <span className="bmi-value text-center">{bmiValue}</span>
-                                    </p>
-                                    <p>
-                                        <span className="bmi-message text-center">{bmiMessage}</span>
-                                    </p>
-                                </div>
-                            )}
-                        </div>
-                    </div>
+
+                        <button
+                            type='button'
+                            className="calculate-btn flex w-full justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+                            onClick={calculateBmi}
+                        >
+                            محاسبه شاخص توده بدنی
+                        </button>
+                        {bmiValue && bmiMessage && (
+                            <div dir='rtl'>
+                                <p>
+                                    شاخص توده بدنی شما: <span className="bmi-value">{bmiValue}</span>
+                                </p>
+                                <p>
+                                    <span className="bmi-message">{bmiMessage}</span>
+                                </p>
+                            </div>
+                        )}
+                    </form>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
+
+    </>
     );
 }
 
--- frontend/vite-project/src/components/pages/UserPage/EditInfo.tsx
+++ frontend/vite-project/src/components/pages/UserPage/EditInfo.tsx
@@ -1,8 +1,226 @@
-function EditInfoForm() {
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import '../../../App.css'
+import Gradient from '../../gradient';
+
+const EditInfo: React.FC = () => {
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        password2: '',
+        gender: 'Male',
+        weight: '',
+        height: '',
+        age: '',
+        exercise_level: 'little or no exercise',
+    });
+
+    const [passwordsMatch, setPasswordsMatch] = useState(true);
+    const [uniqueEmail, setUniqueEmail] = useState(true);
+    const [ageLimit, setAgeLimit] = useState(true);
+    const [weightLimit, setWeightLimit] = useState(true);
+    const [heightLimit, setHeightLimit] = useState(true);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        if (name === 'password2') {
+            if (formData.password !== value && formData.password2 != '') {
+                setPasswordsMatch(false);
+            } else {
+                setPasswordsMatch(true);
+            }
+        }
+        else if (name == 'email') {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (emailRegex.test(value)) {
+                setUniqueEmail(true);
+            } else {
+                setUniqueEmail(false);
+            }
+        }
+
+        if (name == 'weight') {
+            setWeightLimit(true);
+        }
+        else if (name == 'height') {
+            setHeightLimit(true);
+        }
+        else if (name == 'age') {
+            setAgeLimit(true);
+        }
+
+        if (name == "gender") {
+            console.log(value);
+        }
+
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (passwordsMatch) {
+            try {
+                const modifiedFormData = {
+                    "user_id": formData.email,
+                    "first_name": formData.first_name,
+                    "last_name": formData.last_name,
+                    "age": parseInt(formData.age),
+                    "weight": parseInt(formData.weight),
+                    "height": parseInt(formData.height),
+                    "activity_level": formData.exercise_level,
+                    "password": formData.password,
+                    "password2": formData.password2,
+                    "gender": formData.gender,
+                };
+                const response = await axios.post('http://localhost:4050/api/user/register/', modifiedFormData,
+                    {
+                        headers: {
+                            "Content-Type": 'application/json'
+                        }
+                    }
+                )
+            } catch (error) {
+                if (error.response.data.hasOwnProperty("user_id")) {
+                    setUniqueEmail(false);
+                }
+                if (error.response.data.hasOwnProperty("weight")) {
+                    setWeightLimit(false);
+                }
+                if (error.response.data.hasOwnProperty("height")) {
+                    setHeightLimit(false);
+                }
+                if (error.response.data.hasOwnProperty("age")) {
+                    setAgeLimit(false);
+                }
+            }
+        }
+    };
 
     return (
-        <p>edit info</p>
-    )
-}
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 font">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 dir="rtl" className="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    تغییر مشخصات
+                </h2>
+            </div>
 
-export default EditInfoForm;
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+
+                    <div dir="rtl">
+                        <label htmlFor="gender" className="block text-sm font-medium leading-6 text-gray-900">
+                            جنسیت
+                        </label>
+                        <div className="mt-2">
+                            <select
+                                id="gender"
+                                name="gender"
+                                value={formData.gender}
+                                onChange={handleChange}
+                                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                            >
+                                <option value="Male">مرد</option>
+                                <option value="Female">زن</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div dir="rtl">
+                        <label htmlFor="weight" className="block text-sm font-medium leading-6 text-gray-900">
+                            وزن (کیلوگرم)
+                        </label>
+                        <div className={weightLimit ? "mt-2" : "mt-2"} style={weightLimit ? {} : { border: '2px solid red', borderRadius: '5px' }}>
+                            <input
+                                id="weight"
+                                name="weight"
+                                type="number"
+                                value={formData.weight}
+                                onChange={handleChange}
+                                className={weightLimit ?
+                                    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" :
+                                    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                }
+                            />
+                        </div>
+                    </div>
+
+                    <div dir="rtl">
+                        <label htmlFor="height" className="block text-sm font-medium leading-6 text-gray-900">
+                            قد (سانتی متر)
+                        </label>
+                        <div className={heightLimit ? "mt-2" : "mt-2"} style={heightLimit ? {} : { border: '2px solid red', borderRadius: '5px' }}>
+                            <input
+                                id="height"
+                                name="height"
+                                type="number"
+                                value={formData.height}
+                                onChange={handleChange}
+                                className={heightLimit ?
+                                    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" :
+                                    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                }
+                            />
+                        </div>
+                    </div>
+
+                    <div dir="rtl">
+                        <label htmlFor="height" className="block text-sm font-medium leading-6 text-gray-900">
+                            سن
+                        </label>
+                        <div className={ageLimit ? "mt-2" : "mt-2"} style={ageLimit ? {} : { border: '2px solid red', borderRadius: '5px' }}>
+                            <input
+                                id="age"
+                                name="age"
+                                type="number"
+                                value={formData.age}
+                                onChange={handleChange}
+                                className={ageLimit ?
+                                    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" :
+                                    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                }
+                            />
+                        </div>
+                    </div>
+
+                    <div dir="rtl">
+                        <label htmlFor="exercise_level" className="block text-sm font-medium leading-6 text-gray-900">
+                            میزان فعالیت
+                        </label>
+                        <div className="mt-2">
+                            <select
+                                id="exercise_level"
+                                name="exercise_level"
+                                value={formData.exercise_level}
+                                onChange={handleChange}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            >
+                                <option value="little or no exercise">هیچ</option>
+                                <option value="light exercise">کم</option>
+                                <option value="moderate exercise">متوسط</option>
+                                <option value="hard exercise">بالا</option>
+                                <option value="very hard exercise">بسیار بالا</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            ثبت تغییرات
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default EditInfo;
